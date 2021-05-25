090a6c84e36082c53e3b761939931fd9c238675c
Kendall Moraski <71110821+kendimoraski@users.noreply.github.com>
2021-05-25 10:06:28 -0400

    Time: 92 ms (62.22%), Space: 40.6 MB (35.28%) - LeetHub

--- remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
+++ remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -10,32 +10,30 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-    // 1. initialize an answer node to store the distinct elements and ultimately be returned
-    const ans = new ListNode()
-    // 3. move the head's next to the holder
+
     // initialize an iterator for the nodes
-    let prev = ans
+    let prev = head
+    // initialize a variable to hold the next value
+    let next
     
-    if (!head) return ans.next
+    if (!head) return head
     
-    // 4. for the very first index, move the head to the answer's next
-    prev.next = head
-    // 5. move the holder's contents back to the head
-    head = head.next
-    
-    while (head) {
-     // 7. each time, compare the head's current value with the value of the node on ans.next
-        if (head.val !== prev.next.val) {
-    // 7a. if it's not the same, move it to the answer
-            prev = prev.next
-            prev.next = head
+    // while prev exists
+    while (prev) {
+     // each time, compare the current value with its next value
+     // if they are the same,
+        if (prev.next && prev.val === prev.next.val) {
+            // move the next value and beyond to the `next` storage variable (or somewhere else?) and then move that variable's `.next` back to `prev`'s `.next`
+            next = prev.next
+            while (next && next.val === prev.val) { 
+                next = next.next
+            }
+            prev.next = next
         }
-    // 8. move the holder contents to the head again (to "override" any duplicates?)
-        head = head.next
-    // 9. repeat until the holder and head are empty
+        // move the holder contents to the head again (to "override" any duplicates?)
+        prev = prev.next
     }
-    // make sure the end of prev gets cut off
-    prev.next.next = null
-    // 10. return answer node
-    return ans.next
+
+    // return the newly edited head
+    return head
 };
