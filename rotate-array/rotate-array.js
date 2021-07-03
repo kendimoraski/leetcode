3d748ecf999fdca1d1ec78d9233619ce136918d9
Kendall Moraski <71110821+kendimoraski@users.noreply.github.com>
2021-07-03 10:38:40 -0400

    Time: 128 ms (26.85%), Space: 45.7 MB (98.55%) - LeetHub

--- rotate-array/rotate-array.js
+++ rotate-array/rotate-array.js
@@ -4,27 +4,18 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    // edge case
-    if (k === 0 || nums.length === 1) return nums
-    if (k > nums.length) {
-        k = k % nums.length
+    k = k % nums.length
+    let count = 0
+    for (let start = 0; count < nums.length; start++) {
+        let current = start
+        let prev = nums[start]
+        do {
+            let next = (current + k) % nums.length
+            let temp = nums[next]
+            nums[next] = prev
+            prev = temp
+            current = next
+            count++
+        } while (start !== current)
     }
-    const ans = []
-    let pointer = nums.length - k
-    // this for loop simply serves to tell us how many times to iterate
-    for (let _ = 0; _ < nums.length; _++) {
-        // iterate to the end, and push them to an answer array
-        ans.push(nums[pointer])
-        // iterate pointer
-        pointer++
-        // move the pointer to the beginning of nums and keep adding until you hit k again   
-        if (pointer === nums.length) {
-            pointer = 0
-        }
-    }
-    for (let i = 0; i < nums.length; i++) {
-        nums[i] = ans[i]
-    }
-    return nums
-};
-// 20 min up
+}
