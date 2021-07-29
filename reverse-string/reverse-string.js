48a87d4c00753016880e34feae6376814b817535
Kendall Moraski <71110821+kendimoraski@users.noreply.github.com>
2021-07-29 11:02:24 -0400

    Time: 112 ms (47.34%), Space: 47.6 MB (13.55%) - LeetHub

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -2,14 +2,30 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
+
 var reverseString = function(s) {
-    let n = 1
-    for (let i = 0; i < s.length / 2; i++) {
-        let temp = s[s.length - n]
-        s[s.length - n] = s[i]
-        s[i] = temp
-        n++
+    // two pointers (iterative)
+    /*
+    let left = 0
+    let right = s.length - 1
+    let temp = ''
+    while (left <= right) {
+        temp = s[left]
+        s[left] = s[right]
+        s[right] = temp
+        left++
+        right--
     }
-    // console.log(s)
-}; // Time O(n) where n is the length of s / 2, Space O(1)
-// if we didn't need to keep track of every letter in s, we would want to try to optimize this to O(log n) by getting rid of some amount of letters each time inside of a while loop
+    */
+    // recursive
+    var helper = function(left, right) {
+        // base case
+        if (left < right) {
+            let temp = s[left]
+            s[left++] = s[right]
+            s[right--] = temp
+            helper(left, right)
+        }
+    }
+    helper(0, s.length - 1)
+}
