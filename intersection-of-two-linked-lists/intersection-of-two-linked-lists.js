a317e534efa118682fc3071653036cc0807077bc
Kendall Moraski <71110821+kendimoraski@users.noreply.github.com>
2021-08-06 13:26:10 -0400

    Time: 108 ms (29.77%), Space: 46.6 MB (21.90%) - LeetHub

---
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    const headAArray = []
+    let tempA = headA
+    while (tempA) {
+        headAArray.push(tempA.val)
+        tempA = tempA.next
+    }
+    const headBArray = []
+    let tempB = headB
+    while (tempB) {
+        headBArray.push(tempB.val)
+        tempB = tempB.next
+    }
+    
+    tempA = headA
+    tempB = headB
+    if (headAArray.length > headBArray.length) {
+        for (let i = 0; i < headAArray.length - headBArray.length; i++) {
+            tempA = tempA.next
+        }
+        while (tempA && tempB) {
+            if (tempA === tempB) {
+                    return tempA
+            } else {
+                    tempA = tempA.next
+                    tempB = tempB.next
+            }
+        }
+        return null
+    } else if (headBArray.length > headAArray.length) {
+        for (let i = 0; i < headBArray.length - headAArray.length; i++) {
+            tempB = tempB.next
+        }
+        while (tempA && tempB) {
+             if (tempA === tempB) {
+                    return tempA
+                } else {
+                    tempA = tempA.next
+                    tempB = tempB.next
+                }
+            }
+        return null
+    } else {
+        while (tempA && tempB) {
+             if (tempA === tempB) {
+                    return tempA
+                } else {
+                    tempA = tempA.next
+                    tempB = tempB.next
+                }
+            }
+        return null
+    }
+};
