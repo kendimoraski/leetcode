af4dc711d33d5ae0bb8821b241a38043ccfb0a0e
Kendall Moraski <71110821+kendimoraski@users.noreply.github.com>
2021-08-06 17:50:27 -0400

    Time: 84 ms (66.46%), Space: 40.3 MB (81.04%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.js
+++ merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -11,7 +11,8 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-
+    /*
+    // ITERATIVE
     // 2. initialize an answer node to add to and ultimately return
     const ans = new ListNode()
     // use prev as a copy to hold onto the ans values for iterating; this is not a hook, this is a node
@@ -50,4 +51,15 @@ var mergeTwoLists = function(l1, l2) {
     
     // 8. return merged list
     return ans.next
+*/
+    // RECURSIVE
+    if (!l1) return l2
+    if (!l2) return l1
+    if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next)
+        return l2
+    }
 };
