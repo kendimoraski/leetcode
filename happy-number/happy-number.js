e41952ccfd878941dcb32d0251a78f800735de42
Kendall Moraski <71110821+kendimoraski@users.noreply.github.com>
2021-08-13 11:44:08 -0400

    Time: 84 ms (51.74%), Space: 40.4 MB (70.32%) - LeetHub

--- happy-number/happy-number.js
+++ happy-number/happy-number.js
@@ -12,22 +12,24 @@ var isHappy = function(n) {
     //// base case is when sum equals 1 or we've seen the number, meaning it's already been stored in memo b/c we've gone through all possible combinations between 1 and 162
     // repeat the same process until the sum equals 1
     
-     const memo = {}
+     const mySet = new Set()
      
      function recurseMe(x) {
-         let sum = 0
+        let sum = 0
         while (x !== 0) {
-         sum += (x % 10) ** 2
-         x = Math.floor(x / 10)
+            sum += (x % 10) ** 2
+            x = Math.floor(x / 10)
         }
         if (sum === 1) {
             return true
-        } else if (memo[sum]) {
+        } else if (mySet.has(sum)) {
             return false
         } else {
-            memo[sum] = true
+            mySet.add(sum)
             return recurseMe(sum)
         }
      }
     return recurseMe(n)
 };
+// Time O(n) because the while loop runs worst-case scenario 10 times based off of the constraints; the max number of runs through the while loop is 10 meaning the while loop itself is O(1)
+// Space O(2n) which is O(n)
